5e40c93747265ec576bf2127e93c0fb5b4826418
Fotis Gogos <83247314+FotisGogos@users.noreply.github.com>
2021-06-05 00:12:41 +0200

    1.2 Task new pokemon app

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood ='Lasagna';
-document.write(favoriteFood);
+//lists and names of which of Pokemon
+let  pokemonList = [{name:  "Bulbasaur",  height: 0.7,  types:  ['grass', 'poison']},
+                    {name:  "Squirtl",  height: 0.5,  type:  ['water']},
+                    {name:  "Slowpoke", height: 1.2,  types: ['psychic', 'water']}];
+
+console.log(pokemonList);
