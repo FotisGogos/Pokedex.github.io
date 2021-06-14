11f4035fd4aa53081b902aafa9a1cb599ff90b27
Fotis Gogos <83247314+FotisGogos@users.noreply.github.com>
2021-06-14 20:25:08 +0200

    Updated script 1.5 part  2

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,40 @@
-//lists and names of which of Pokemon
-let  pokemonList = [{name:  "Bulbasaur",  height: 0.7,  types:  ['grass', 'poison']},
-                    {name:  "Squirtl",  height: 0.5,  types:  ['water']},
-                    {name:  "Slowpoke", height: 1.2,  types: ['psychic', 'water']}];
+function printPokemonList(list) {
+  document.write("<br>")
+  for (let  i = 0; i < list.length; i++) {
+    if (list[i].height > 1) {
+        document.write("<p style='text-align:center'>" +  list[i] ["name"] + " " + "(height:" + " " + list[i].height + ") - " + "Wow thas big!")
+        document.createElement ("center")
 
-console.log(pokemonList);
-//ends here
-
-document.write("<br>")
-for (let  i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1) {
-      document.write("<p style='text-align:center'>" +  pokemonList[i] ["name"] + " " + "(height:" + " " + pokemonList[i].height + ") - " + "Wow thas big!")
+    }
+    else {
+      document.write("<p style='text-align:center'>" +  list[i] ["name"] + " " + "(height:" + " " + list[i].height + ")"  )
       document.createElement ("center")
 
-  }
-  else {
-    document.write("<p style='text-align:center'>" +  pokemonList[i] ["name"] + " " + "(height:" + " " + pokemonList[i].height + ")"  )
-    document.createElement ("center")
-
+    }
   }
 }
+
+let pokemonRepository = (function () {
+  let pokemonList = [{name:  "Bulbasaur",  height: 0.7,  types:  ['grass', 'poison']},
+                        {name:  "Squirtl",  height: 0.5,  types:  ['water']},
+                        {name:  "Slowpoke", height: 1.2,  types: ['psychic', 'water']}];
+
+  function add(item) {
+    pokemonList.push(item);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonList = pokemonRepository.getAll()
+
+pokemonList.forEach(function(pokemon) {
+  console.log(pokemon.name + pokemon.height );
+});
