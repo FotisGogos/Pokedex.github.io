244380398935246f3dcd967f208454642192acd6
Fotis Gogos <83247314+FotisGogos@users.noreply.github.com>
2021-06-26 15:39:19 +0200

    Replace the static list with a complete API list

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonRepository = (function () {
-let pokemonList = [{name:  "Bulbasaur",  height: 0.7,  types:  ['grass', 'poison']},
-                        {name:  "Squirtl",  height: 0.5,  types:  ['water']},
-                        {name:  "Slowpoke", height: 1.2,  types: ['psychic', 'water']}];
+  let pokemonList = [];
+  // API link
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150'; 
 
   function add(item) {
     if(typeof item === 'object'){
@@ -22,6 +22,38 @@ let pokemonList = [{name:  "Bulbasaur",  height: 0.7,  types:  ['grass', 'poison
     listPokemon.appendChild(button)
     pokemonList.appendChild(listPokemon)
   }
+      // Load list of pokemons without details 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  // Load list of pokemons with details 
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
 
   function addEventListenerToButton(button, pokemon){
     button.addEventListener('click', function () {
@@ -29,8 +61,10 @@ let pokemonList = [{name:  "Bulbasaur",  height: 0.7,  types:  ['grass', 'poison
     });
   }
 
-  function showDetails(pokemon){
-    console.log(pokemon.name);
+  function showDetails(item){
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
   }
 
   function getAll() {
@@ -40,19 +74,16 @@ let pokemonList = [{name:  "Bulbasaur",  height: 0.7,  types:  ['grass', 'poison
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
+    
   };
 })();
 
-pokemonList = pokemonRepository.getAll()
-pokemonList.forEach(function(pokemon) {
-  console.log(pokemon.name + pokemon.height );
-});
-
-console.log(pokemonRepository.getAll())
-pokemonRepository.add({name: "Mew", height: 1.3, types: [ "poison" ]});
-console.log(pokemonRepository.getAll())
-
-pokemonRepository.getAll().forEach(function(pokemon){
-  pokemonRepository.addListItem(pokemon)
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon)
+  }); 
 }); 
